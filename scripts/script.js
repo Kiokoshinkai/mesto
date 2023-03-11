3e6edb1411ee751f5c989ff51f38dd2d63623ab1
Kiokoshinkai <polnochnamarse@gmail.com>
2023-03-11 17:53:09 +0300

    hotfix: after 1st review

--- scripts/script.js
+++ scripts/script.js
@@ -2,14 +2,12 @@
 const contentElement = document.querySelector('.content');
 const profileElement = contentElement.querySelector('.profile');
 const popupElement = document.querySelector('.popup');
-const elementItem = document.querySelector('.elements');
+const popupForm = popupElement.querySelector('.popup__form');
 const profileName = profileElement.querySelector('.profile__name');
 const profileStatus = profileElement.querySelector('.profile__status');
 
-const editButton = profileElement.querySelector('.edit-button');
-const addButton = profileElement.querySelector('.add-button');
+const profileButton = profileElement.querySelector('.profile__edit-button');
 const closeButton = popupElement.querySelector('.popup__close-btn');
-
 
 const formName = popupElement.querySelector('.popup__form-item_el_name');
 const formStatus = popupElement.querySelector('.popup__form-item_el_status');
@@ -18,24 +16,23 @@ const formStatus = popupElement.querySelector('.popup__form-item_el_status');
 function editProfile() {
   if (popupElement.classList.contains('popup_opened') === false) {
     popupElement.classList.add('popup_opened');
-  } else {
-    popupElement.classList.remove('popup_opened');
   }
+  //данные забираются в момент открытия редактора профиля через функцию:
   getNameAndStatusForForm();
 }
 
-// Вызов функции по нажатию на кнопку (открыть/закрыть редактор профиля)
-editButton.addEventListener('click', editProfile);
-closeButton.addEventListener('click', editProfile);
+// Функция закрытия редактора профиля (если класс есть, он удаляется)
+function closeProfile() {
+  if (popupElement.classList.contains('popup_opened') === true) {
+    popupElement.classList.remove('popup_opened');
+  }
+}
 
 // Функция сохранения данных профиля в полях формы
 function getNameAndStatusForForm() {
   formName.value = profileName.textContent;
   formStatus.value = profileStatus.textContent;
 }
-
-// При закрытии попапа на крест вызывает функцию и сохраняет данные из профиля
-closeButton.addEventListener('click', getNameAndStatusForForm);
 
 // функция перезаписывает данные профиля с полей ввода
 function handleFormSubmit (evt) {
@@ -45,23 +42,26 @@ function handleFormSubmit (evt) {
   profileStatus.textContent = formStatus.value;
 
   // нужно вызывать для пддержания логики закрытия попапа
-  editProfile();
+  closeProfile();
 }
 
-popupElement.addEventListener('submit', handleFormSubmit);
+// Вызов функции по нажатию на кнопку (открыть/закрыть редактор профиля)
+profileButton.addEventListener('click', editProfile);
+closeButton.addEventListener('click', closeProfile);
+// слушатель на форму т.к отправляем данные формы на сервер
+popupForm.addEventListener('submit', handleFormSubmit);
 
 // обработчик черных сердец. На вход querySelectorAll приходит массив, циклом достаем элементы массива и добавляем каждому через лисинер логику(если класс отсутствует, он добавляется)
-const likeButtons = elementItem.querySelectorAll('.card__like');
+// const likeButtons = elementItem.querySelectorAll('.card__like');
 
-for (let i = 0; i < likeButtons.length; i += 1) {
-  likeButtons[i].addEventListener('click', addLike);
-}
+// for (let i = 0; i < likeButtons.length; i += 1) {
+//   likeButtons[i].addEventListener('click', addLike);
+// }
 
-function addLike() {
-  if (this.classList.contains('card__like_active') === false) {
-    this.classList.add('card__like_active');
-  } else {
-    this.classList.remove('card__like_active');
-  }
-}
-
+// function addLike() {
+//   if (this.classList.contains('card__like_active') === false) {
+//     this.classList.add('card__like_active');
+//   } else {
+//     this.classList.remove('card__like_active');
+//   }
+// }
