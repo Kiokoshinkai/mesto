b919058093595b245e63e0faf6a1973c3167d99e
Kiokoshinkai <polnochnamarse@gmail.com>
2023-03-21 01:17:12 +0300

    feat: add cards from array

--- scripts/script.js
+++ scripts/script.js
@@ -12,6 +12,10 @@ const closeButton = popupElement.querySelector('.popup__close-btn');
 const formName = popupElement.querySelector('.popup__form-item_el_name');
 const formStatus = popupElement.querySelector('.popup__form-item_el_status');
 
+//получаем доступ к секции карточек, к темплейт элементу карточки
+const cardElements = contentElement.querySelector('.elements');
+const cardTemplate = contentElement.querySelector('#card').content;
+
 // Функция вызова редактора профиля
 function editProfile() {
     popupElement.classList.add('popup_opened');
@@ -51,29 +55,62 @@ popupForm.addEventListener('submit', handleFormSubmit);
 const initialCards = [
   {
     name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    link: './images/element_arkhyz.jpg'
   },
   {
     name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    link: './images/element_chelyabinsk-oblast.jpg'
   },
   {
     name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    link: './images/element_ivanovo.jpg'
   },
   {
     name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    link: './images/element_kamchatka.jpg'
   },
   {
     name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    link: './images/element_kholmogorsky-rayon.jpg'
   },
   {
     name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    link: './images/element_baikal.jpg'
   }
 ];
+
+//функция клонирует один элемент(карточку) и заносит в него данные из массива, добавляет карточку первым элементом
+function addCardContent(cardInfo) {
+  const templateClone = cardTemplate.querySelector('.card').cloneNode(true);
+  templateClone.querySelector('.card__image').src = cardInfo.link;
+  templateClone.querySelector('.card__image').alt = cardInfo.name;
+  templateClone.querySelector('.card__title').textContent = cardInfo.name;
+
+  cardElements.prepend(templateClone);
+}
+
+//функция перебирает все данные массива и применяет функцию выше
+function addAllCardsFromArray() {
+  initialCards.forEach(arrayEl => addCardContent(arrayEl));
+}
+
+//вызов функции для добавления
+addAllCardsFromArray();
+
+//Другой вариант клона карточек:
+// function cloneCards() {
+//   initialCards.forEach((card) => {
+//     const cardClone = cardTemplate.querySelector('.card').cloneNode(true);
+//     cardClone.querySelector('.card__image').src = card.link;
+//     cardClone.querySelector('.card__image').alt = card.name;
+//     cardClone.querySelector('.card__title').textContent = card.name;
+
+//     cardElements.append(cardClone);
+//   });
+// }
+
+// cloneCards()
+
 
 // обработчик черных сердец. На вход querySelectorAll приходит массив, циклом достаем элементы массива и добавляем каждому через лисинер логику(если класс отсутствует, он добавляется)
 // const likeButtons = elementItem.querySelectorAll('.card__like');
