d759bca0d4f2b595cd9ef0dbbf884bbcc19d1612
Kiokoshinkai <polnochnamarse@gmail.com>
2023-03-22 17:17:09 +0300

    feat: delete card and zoom img

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,7 @@ const contentElement = document.querySelector('.content');
 const profileElement = contentElement.querySelector('.profile');
 const profilePopup = document.querySelector('.popup_place_profile');
 const cardPopup = document.querySelector('.popup_place_card');
+const imagePopup = document.querySelector('.popup_place_image');
 const popupProfileForm = profilePopup.querySelector('.popup__form_el_profile');
 const popupCardForm = cardPopup.querySelector('.popup__form_el_card');
 const profileName = profileElement.querySelector('.profile__name');
@@ -12,39 +13,28 @@ const profileButton = profileElement.querySelector('.profile__edit-button');
 const cardButton = profileElement.querySelector('.profile__add-button');
 const profileCloseButton = profilePopup.querySelector('.popup__close-btn_place_profile');
 const cardCloseButton = cardPopup.querySelector('.popup__close-btn_place_card');
+const imageCloseButton = imagePopup.querySelector('.popup__close-btn_place_image');
 
 const formName = profilePopup.querySelector('.popup__form-item_el_name');
 const formStatus = profilePopup.querySelector('.popup__form-item_el_status');
 const placeName = cardPopup.querySelector('.popup__form-item_el_place-name');
 const placeLink = cardPopup.querySelector('.popup__form-item_el_place-link');
 
-//получаем доступ к секции карточек, к темплейт элементу карточки
+//Получаем доступ к секции карточек, к темплейт элементу карточки
 const cardElements = contentElement.querySelector('.elements');
 const cardTemplate = contentElement.querySelector('.card-template').content;
 
-// Функция вызова редактора профиля
-function editProfile() {
-  profilePopup.classList.add('popup_opened');
-  //данные забираются в момент открытия редактора профиля через функцию:
-  getNameAndStatusForForm();
+//функция открытия попапа с аргументом для переиспользования
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
 }
 
-// Функция закрытия редактора профиля
-function closeProfile() {
-  profilePopup.classList.remove('popup_opened');
+//Функция закрытия попапа с аргументом для переиспользования
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
 }
 
-// Функция вызова окна добавления места
-function addPlace() {
-  cardPopup.classList.add('popup_opened');
-}
-
-// Функция закрытия окна добавления места
-function closePlace() {
-  cardPopup.classList.remove('popup_opened');
-}
-
-// Функция сохранения данных профиля в полях формы
+//Функция сохранения данных профиля в полях формы
 function getNameAndStatusForForm() {
   formName.value = profileName.textContent;
   formStatus.value = profileStatus.textContent;
@@ -58,7 +48,7 @@ function handleFormSubmit (evt) {
   profileStatus.textContent = formStatus.value;
 
   // нужно вызывать для пддержания логики закрытия попапа
-  closeProfile();
+  closePopup(profilePopup);
 }
 
 // функция добавления карточки
@@ -70,16 +60,27 @@ function savePlaceSubmit (evt) {
     link: placeLink.value
   };
   //вызываем функцию для клона карточки и передаем в нее параметры с полей ввода
-  addCardContent(placeValueObject);
+  createCard(placeValueObject);
   // нужно вызывать для пддержания логики закрытия попапа
-  closePlace();
+  closePopup(cardPopup);
 }
 
 // Вызов функции по нажатию на кнопку (открыть/закрыть редактор профиля, добавить место)
-profileButton.addEventListener('click', editProfile);
-cardButton.addEventListener('click', addPlace);
-profileCloseButton.addEventListener('click', closeProfile);
-cardCloseButton.addEventListener('click' , closePlace);
+profileButton.addEventListener('click', () => {
+  openPopup(profilePopup), getNameAndStatusForForm();//перенос данных профиля в форму
+});
+cardButton.addEventListener('click', () => {
+  openPopup(cardPopup);
+});
+profileCloseButton.addEventListener('click', () => {
+  closePopup(profilePopup);
+});
+cardCloseButton.addEventListener('click' , () => {
+  closePopup(cardPopup);
+});
+imageCloseButton.addEventListener('click', () => {
+  closePopup(imagePopup);
+});
 // слушатель на форму т.к отправляем данные формы на сервер
 popupProfileForm.addEventListener('submit', handleFormSubmit);
 popupCardForm.addEventListener('submit', savePlaceSubmit);
@@ -113,39 +114,33 @@ const initialCards = [
 ];
 
 //функция клонирует один элемент(карточку) и заносит в него данные из массива, добавляет карточку первым элементом
-function addCardContent(cardInfo) {
+function createCard(cardInfo) {
   const templateClone = cardTemplate.querySelector('.card').cloneNode(true);
   templateClone.querySelector('.card__image').src = cardInfo.link;
   templateClone.querySelector('.card__image').alt = cardInfo.name;
   templateClone.querySelector('.card__title').textContent = cardInfo.name;
+  //обработчик черных сердец
+  templateClone.querySelector('.card__like').addEventListener('click', (evt) => {
+    evt.target.classList.toggle('card__like_active');
+    });
+  //удаление карточек
+  templateClone.querySelector('.card__trash').addEventListener('click', (evt) => {
+    evt.target.closest('.card').remove();
+    });
+  //обработка клика по картинке
+  templateClone.querySelector('.card__image').addEventListener('click', () => {
+    imagePopup.querySelector('.popup__image').src = cardInfo.link;
+    imagePopup.querySelector('.popup__image').alt = cardInfo.name;
+    imagePopup.querySelector('.popup__figcaption').textContent = cardInfo.name;
+    openPopup(imagePopup)
+  });
 
   cardElements.prepend(templateClone);
-
-  //обработчик черных сердец
-  const likeButton = contentElement.querySelector('.card__like');
-  likeButton.addEventListener('click', (evt) => {
-    evt.target.classList.toggle('card__like_active');
-  });
 }
 
 //функция перебирает все данные массива и применяет функцию выше
 function addAllCardsFromArray() {
-  initialCards.forEach(arrayEl => addCardContent(arrayEl));
+  initialCards.forEach(arrayEl => createCard(arrayEl));
 }
-
 //вызов функции для добавления всех карточек из массива
 addAllCardsFromArray();
-
-//Другой вариант клона карточек:
-// function cloneCards() {
-//   initialCards.forEach((card) => {
-//     const cardClone = cardTemplate.querySelector('.card').cloneNode(true);
-//     cardClone.querySelector('.card__image').src = card.link;
-//     cardClone.querySelector('.card__image').alt = card.name;
-//     cardClone.querySelector('.card__title').textContent = card.name;
-
-//     cardElements.append(cardClone);
-//   });
-// }
-
-// cloneCards()
