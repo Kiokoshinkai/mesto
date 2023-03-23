1821a0af75db3688eda0579e4170481d2df4809e
Kiokoshinkai <polnochnamarse@gmail.com>
2023-03-23 15:21:09 +0300

    bugfix:1st review

---
+++ scripts/cards.js
@@ -0,0 +1,27 @@
+//массив для карточек
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: './images/element_arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: './images/element_chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: './images/element_ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: './images/element_kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: './images/element_kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: './images/element_baikal.jpg'
+  }
+];
--- scripts/script.js
+++ scripts/script.js
@@ -4,23 +4,24 @@ const profileElement = contentElement.querySelector('.profile');
 const profileName = profileElement.querySelector('.profile__name');
 const profileStatus = profileElement.querySelector('.profile__status');
 //попап
-const profilePopup = document.querySelector('.popup_place_profile');
-const cardPopup = document.querySelector('.popup_place_card');
-const imagePopup = document.querySelector('.popup_place_image');
-const popupProfileForm = profilePopup.querySelector('.popup__form_el_profile');
-const popupCardForm = cardPopup.querySelector('.popup__form_el_card');
-const formName = profilePopup.querySelector('.popup__form-item_el_name');
-const formStatus = profilePopup.querySelector('.popup__form-item_el_status');
-const placeName = cardPopup.querySelector('.popup__form-item_el_place-name');
-const placeLink = cardPopup.querySelector('.popup__form-item_el_place-link');
+const profileFormPopup = document.querySelector('.popup_place_profile');
+const placeFormPopup = document.querySelector('.popup_place_card');
+const imageZoomPopup = document.querySelector('.popup_place_image');
+const placeImage = imageZoomPopup.querySelector('.popup__image');
+const popupFigcaption = imageZoomPopup.querySelector('.popup__figcaption');
+const profileForm = profileFormPopup.querySelector('.popup__form_el_profile');
+const placeForm = placeFormPopup.querySelector('.popup__form_el_card');
+//поля форм
+const formProfileFieldName = profileFormPopup.querySelector('.popup__form-item_el_name');
+const formProfileFieldStatus = profileFormPopup.querySelector('.popup__form-item_el_status');
+const formPlaceFieldPlace = placeFormPopup.querySelector('.popup__form-item_el_place-name');
+const formPlaceFieldLink = placeFormPopup.querySelector('.popup__form-item_el_place-link');
 //кнопки
 const profileButton = profileElement.querySelector('.profile__edit-button');
 const cardButton = profileElement.querySelector('.profile__add-button');
-const profileCloseButton = profilePopup.querySelector('.popup__close-btn_place_profile');
-const cardCloseButton = cardPopup.querySelector('.popup__close-btn_place_card');
-const imageCloseButton = imagePopup.querySelector('.popup__close-btn_place_image');
+const closeButtons = document.querySelectorAll('.popup__close-btn');
 //получить доступ к секции карточек, к темплейт элементу карточки
-const cardElements = contentElement.querySelector('.elements');
+const cardsContainer = contentElement.querySelector('.elements');
 const cardTemplate = contentElement.querySelector('.card-template').content;
 
 //функция открытия попапа с аргументом для переиспользования
@@ -33,65 +34,39 @@ function closePopup(popup) {
 }
 //функция сохранения данных профиля в полях формы
 function getNameAndStatusForForm() {
-  formName.value = profileName.textContent;
-  formStatus.value = profileStatus.textContent;
+  formProfileFieldName.value = profileName.textContent;
+  formProfileFieldStatus.value = profileStatus.textContent;
 }
 //функция перезаписывает данные профиля с полей ввода
-function handleFormSubmit (evt) {
+function handleProfileFormSubmit (evt) {
   evt.preventDefault();
 
-  profileName.textContent = formName.value;
-  profileStatus.textContent = formStatus.value;
+  profileName.textContent = formProfileFieldName.value;
+  profileStatus.textContent = formProfileFieldStatus.value;
   //нужно вызывать для пддержания логики закрытия попапа
-  closePopup(profilePopup);
+  closePopup(profileFormPopup);
 }
 //функция добавления карточки
-function savePlaceSubmit (evt) {
+function handlePlaceFormSubmit (evt) {
   evt.preventDefault();
 
   const placeValueObject = {
-    name: placeName.value,
-    link: placeLink.value
+    name: formPlaceFieldPlace.value,
+    link: formPlaceFieldLink.value
   };
-  //вызываем функцию для клона карточки и передаем в нее параметры с полей ввода
-  createCard(placeValueObject);
+  //добавляем карточку на переднюю позицю
+  cardsContainer.prepend(createCard(placeValueObject));
+  evt.target.reset();
   //нужно вызывать для пддержания логики закрытия попапа
-  closePopup(cardPopup);
+  closePopup(placeFormPopup);
 }
-
-//массив для карточек
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: './images/element_arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: './images/element_chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: './images/element_ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: './images/element_kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: './images/element_kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: './images/element_baikal.jpg'
-  }
-];
 
 //функция клонирует один элемент(карточку) и заносит в него данные из массива, добавляет карточку первым элементом
 function createCard(cardInfo) {
   const templateClone = cardTemplate.querySelector('.card').cloneNode(true);
-  templateClone.querySelector('.card__image').src = cardInfo.link;
-  templateClone.querySelector('.card__image').alt = cardInfo.name;
+  const cardImageClone = templateClone.querySelector('.card__image');
+  cardImageClone.src = cardInfo.link;
+  cardImageClone.alt = cardInfo.name;
   templateClone.querySelector('.card__title').textContent = cardInfo.name;
   //обработчик черных сердец
   templateClone.querySelector('.card__like').addEventListener('click', (evt) => {
@@ -102,39 +77,37 @@ function createCard(cardInfo) {
     evt.target.closest('.card').remove();
     });
   //обработка клика по картинке
-  templateClone.querySelector('.card__image').addEventListener('click', () => {
-    imagePopup.querySelector('.popup__image').src = cardInfo.link;
-    imagePopup.querySelector('.popup__image').alt = cardInfo.name;
-    imagePopup.querySelector('.popup__figcaption').textContent = cardInfo.name;
-    openPopup(imagePopup)
+  cardImageClone.addEventListener('click', () => {
+    placeImage.src = cardInfo.link;
+    placeImage.alt = cardInfo.name;
+    popupFigcaption.textContent = cardInfo.name;
+    openPopup(imageZoomPopup)
   });
 
-  cardElements.prepend(templateClone);
+  return templateClone;
 }
 
-//функция перебирает все данные массива и применяет функцию выше
+//функция перебирает все данные массива и добавляет карточки через функцию createCard
 function addAllCardsFromArray() {
-  initialCards.forEach(arrayEl => createCard(arrayEl));
+  initialCards.forEach(cardInfo => {
+  cardsContainer.append(createCard(cardInfo));
+  });
 }
 //вызов функции для добавления всех карточек из массива
 addAllCardsFromArray();
 
 //вызов функции по нажатию на кнопку (открыть/закрыть редактор профиля, добавить место)
 profileButton.addEventListener('click', () => {
-  openPopup(profilePopup), getNameAndStatusForForm();//перенос данных профиля в форму
+  openPopup(profileFormPopup), getNameAndStatusForForm();//перенос данных профиля в форму
 });
 cardButton.addEventListener('click', () => {
-  openPopup(cardPopup);
+  openPopup(placeFormPopup);
 });
-profileCloseButton.addEventListener('click', () => {
-  closePopup(profilePopup);
-});
-cardCloseButton.addEventListener('click' , () => {
-  closePopup(cardPopup);
-});
-imageCloseButton.addEventListener('click', () => {
-  closePopup(imagePopup);
+//обработчик всех кнопок закрытия
+closeButtons.forEach(button => {
+  const buttonsPopup = button.closest('.popup');
+  button.addEventListener('click', () => closePopup(buttonsPopup));
 });
 //слушатель на форму т.к отправляем данные формы на сервер
-popupProfileForm.addEventListener('submit', handleFormSubmit);
-popupCardForm.addEventListener('submit', savePlaceSubmit);
+profileForm.addEventListener('submit', handleProfileFormSubmit);
+placeForm.addEventListener('submit', handlePlaceFormSubmit);
